0decb182b360a517f95d028b619c53bfe4cec137
Claudio Popolani <cpopolan@c2r6p9.42firenze.it>
2023-08-01 10:08:57 +0200

    1 aug morning

--- noder_utils.c
+++ noder_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   noder_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpopolan <cpopolan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rkhinchi <rkhinchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 11:21:38 by cpopolan          #+#    #+#             */
-/*   Updated: 2023/07/13 13:11:25 by cpopolan         ###   ########.fr       */
+/*   Updated: 2023/07/31 19:03:42 by rkhinchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,17 @@ t_e_type ft_node_builtin_typer(char	*token)
 {
 	t_e_type	type;
 
-	if (ft_strcmp(token, "echo") == 0)
+	if (ft_strcmp01(token, "echo") == 0)
         type = BUILTIN;
-    else if (ft_strcmp(token, "cd") == 0)
+    else if (ft_strcmp01(token, "cd") == 0)
         type = BUILTIN;
-    else if (ft_strcmp(token, "pwd") == 0)
+    else if (ft_strcmp01(token, "pwd") == 0)
         type = BUILTIN;
-    else if (ft_strcmp(token, "export") == 0)
+    else if (ft_strcmp01(token, "export") == 0)
         type = BUILTIN;
-    else if (ft_strcmp(token, "unset") == 0)
+    else if (ft_strcmp01(token, "unset") == 0)
         type = BUILTIN;
-    else if (ft_strcmp(token, "env") == 0)
+    else if (ft_strcmp01(token, "env") == 0)
         type = BUILTIN;
     else
         return(1);
@@ -47,15 +47,15 @@ t_e_type ft_node_redirect_typer(char *token)
 {
 	t_e_type	type;
 
-	if (ft_strcmp(token, "<") == 0)
+	if (ft_strcmp01(token, "<") == 0)
         type = OPEN_FILE;
-    else if (ft_strcmp(token, "<<") == 0)
+    else if (ft_strcmp01(token, "<<") == 0)
         type = HERE_DOC;
-    else if (ft_strcmp(token, "|") == 0)
+    else if (ft_strcmp01(token, "|") == 0)
         type = LIMITOR;
-    else if (ft_strcmp(token, ">") == 0)
+    else if (ft_strcmp01(token, ">") == 0)
         type = CREAT_FILE;
-    else if (ft_strcmp(token, ">>") == 0)
+    else if (ft_strcmp01(token, ">>") == 0)
         type = WRITE_FILE;
     else
         return(1);
